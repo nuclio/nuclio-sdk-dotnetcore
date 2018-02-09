c242afce7a689904c50b8d7eebe029e182f718bf
torosent <torosent@microsoft.com>
2018-02-09 12:35:34 +0800

    Initial impl of dot net core sdk

---
+++ ContextBase.cs
@@ -0,0 +1,6 @@
+namespace nuclio_sdk_dotnetcore
+{
+    public abstract class ContextBase
+    {
+    }
+}
---
+++ ResponseBase.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using MessagePack;
+
+namespace nuclio_sdk_dotnetcore
+{
+    public class ResponseBase
+    {
+        [Key("body")]
+        public byte[] Body { get; set; }
+        
+        [Key("content_type")]
+        public string ContentType { get; set; }
+        
+        [Key("status_code")]
+        public int StatusCode { get; set; }
+        
+        [Key("headers")]
+        public Dictionary<string, object> Headers { get; set; }
+        
+        [Key("body_encoding")]
+        public string BodyEncoding { get { return "base64"; } }
+    }
+}
