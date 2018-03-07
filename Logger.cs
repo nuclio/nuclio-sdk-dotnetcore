1bc1195ca83a33f06ddd0151e7f30f74656351e3
Tomer Rosenthal <torosent@microsoft.com>
2018-03-07 14:12:40 +0200

    Fixed logger With bug and added headers (#3)
    * Initial impl of dot net core sdk
    
    * added formatters
    
    * Removed assembly info
    
    * Update body encoding
    
    * Changed interfaces
    
    * removed serializer
    
    * Added tests
    
    * Added logger and metric impl
    
    * Fixed utc timestamps and base64 string
    
    * Refactored loglevel
    
    * added OrdinalIgnoreCase to headers
    
    * Change serialization to NETJSON
    
    * Update logger interface
    
    * fixed log with bug and added license headers

--- Logger.cs
+++ Logger.cs
@@ -1,3 +1,17 @@
+//  Copyright 2017 The Nuclio Authors.
+// 
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
 using System;
 using System.Collections.Generic;
 using NetJSON;
@@ -18,17 +32,11 @@ namespace nuclio_sdk_dotnetcore
         public string DateTime { get; set; }
 
         [NetJSONProperty("with")]
-        Dictionary<string, object> With { get; set; }
-
-        public Logger()
-        {
-            With = new Dictionary<string, object>();
-            DateTime = (System.DateTime.Now.Subtract(new DateTime(1970, 1, 1))).TotalSeconds.ToString();
-        }
+        public Dictionary<string, object> With { get; set; }
 
         ///<summary>
         /// Log an error message
-        /// e.g. context.Error("%s not responding after %d seconds", dbHost, timeout)
+        /// e.g. context.Logger.Error("{0} not responding after {1} seconds", dbHost, timeout)
         /// <param name="format">Message format</param> 
         /// <param name="args">Formatting arguments</param>    
         ///</summary>
@@ -39,7 +47,7 @@ namespace nuclio_sdk_dotnetcore
 
         ///<summary>
         /// Log a warning message
-        /// e.g. context.Warning("%s %.2f full", "memory", mem_full)
+        /// e.g. context.Logger.Warning("{0} {1} full", "memory", mem_full)
         /// <param name="format">Message format</param> 
         /// <param name="args">Formatting arguments</param>    
         ///</summary>
@@ -50,7 +58,7 @@ namespace nuclio_sdk_dotnetcore
 
         ///<summary>
         /// Log a debug message
-        /// e.g. context.Debug("event with %d bytes", event.GetSize())
+        /// e.g. context.Logger.Debug("event with {0} bytes", event.GetSize())
         /// <param name="format">Message format</param> 
         /// <param name="args">Formatting arguments</param>    
         ///</summary>
@@ -61,7 +69,7 @@ namespace nuclio_sdk_dotnetcore
 
         ///<summary>
         /// Log an info message
-        /// e.g. context.Info("event with %d bytes", event.GetSize())
+        /// e.g. context.Logger.Info("event with {0} bytes", event.GetSize())
         /// <param name="format">Message format</param> 
         /// <param name="args">Formatting arguments</param>    
         ///</summary>
@@ -70,21 +78,45 @@ namespace nuclio_sdk_dotnetcore
             Log(LogLevel.Info, string.Format(format, args));
         }
 
+        ///<summary>
+        /// Log an error message
+        /// e.g. context.Logger.ErrorWith("bad request", "error", "daffy not found", "time", 7)
+        /// <param name="format">Message</param> 
+        /// <param name="args">Formatting arguments</param>    
+        ///</summary>
         public void ErrorWith(string message, params object[] parameters)
         {
             Log(LogLevel.Error, message, EncodeWith(parameters));
         }
 
+        ///<summary>
+        /// Log an error message
+        /// e.g. context.Logger.WarnWith("system overload", "resource", "memory", "used", 0.9)
+        /// <param name="format">Message</param> 
+        /// <param name="args">Formatting arguments</param>    
+        ///</summary>
         public void WarnWith(string message, params object[] parameters)
         {
             Log(LogLevel.Warning, message, EncodeWith(parameters));
         }
 
+        ///<summary>
+        /// Log an error message
+        /// e.g. context.Logger.DebugWith("event", "body_size", 2339, "content-type", "text/plain")
+        /// <param name="format">Message</param> 
+        /// <param name="args">Formatting arguments</param>    
+        ///</summary>
         public void DebugWith(string message, params object[] parameters)
         {
             Log(LogLevel.Debug, message, EncodeWith(parameters));
         }
 
+        ///<summary>
+        /// Log an error message
+        /// e.g. context.Logger.InfoWith("event processed", "time", 0.3, "count", 9009)
+        /// <param name="format">Message</param> 
+        /// <param name="args">Formatting arguments</param>    
+        ///</summary>
         public void InfoWith(string message, params object[] parameters)
         {
             Log(LogLevel.Info, message, EncodeWith(parameters));
@@ -115,6 +147,7 @@ namespace nuclio_sdk_dotnetcore
 
         private void Log(LogLevel level, string message, Dictionary<string, object> with = null)
         {
+            DateTime = (System.DateTime.Now.Subtract(new DateTime(1970, 1, 1))).TotalSeconds.ToString();
             Level = level.ToString().ToLower();
             Message = message;
             if (with == null)
